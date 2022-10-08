c839e9fadc4672d14bbbe617615263462c15b766
Raza <mraza6601@gmail.com>
2022-10-08 18:10:54 +0500

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,3 +5,9 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+//To do Hopefully
+
+//1)Create Auction Page
+//2)Home Page (All active Auctions)
+//3)Specific Auction Page(Where all the previous bids are being shown, and current winner ,and time remaining)

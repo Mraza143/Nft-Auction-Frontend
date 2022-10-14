e533056cdb634f11acb4a3a9e24dda97d8dab650
Raza <mraza6601@gmail.com>
2022-10-14 09:19:09 +0500

    Home Page Auction Component done

---
+++ components/AuctionBox.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import nftAbi from "../constants/Erc721Mock.json"
+import Image from "next/image"
+import { Card, useNotification } from "web3uikit"
+import { ethers } from "ethers"
+
+const truncateStr = (fullStr, strLen) => {
+    if (fullStr.length <= strLen) return fullStr
+    const separator = "..."
+    const seperatorLength = separator.length
+    const charsToShow = strLen - seperatorLength
+    const frontChars = Math.ceil(charsToShow / 2)
+    const backChars = Math.floor(charsToShow / 2)
+    return (
+        fullStr.substring(0, frontChars) +
+        separator +
+        fullStr.substring(fullStr.length - backChars)
+    )
+}
+
+export default function AuctionBox({ price, nftAddress, tokenId,  seller }) {
+    const { isWeb3Enabled, account } = useMoralis()
+    const [imageURI, setImageURI] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
+    const dispatch = useNotification()
+
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+        functionName: "tokenURI",
+        params: {
+            tokenId: tokenId,
+        },
+    })
+
+    
+
+    async function updateUI() {
+        const tokenURI = await getTokenURI()
+        console.log(`The TokenURI is ${tokenURI}`)
+        if (tokenURI) {
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+        }
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const formattedSellerAddress = truncateStr(seller || "", 15)
+
+    const handleCardClick = () => {
+        console.log('c')
+    }
+
+    return (
+        <div>
+            <div>
+                {imageURI ? (
+                    <div>
+                        
+                        <Card
+                            title={tokenName}
+                            description={tokenDescription}
+                            onClick={handleCardClick}
+                        >
+                            <div className="p-2">
+                                <div className="flex flex-col items-end gap-2">
+                                    <div>#{tokenId}</div>
+                                    <div className="italic text-sm">
+                                        Owned by {formattedSellerAddress}
+                                    </div>
+                                    <Image
+                                        loader={() => imageURI}
+                                        src={imageURI}
+                                        height="200"
+                                        width="200"
+                                    />
+                                    <div className="font-bold">
+                                        {ethers.utils.formatUnits(price, "ether")} ETH
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
+}
